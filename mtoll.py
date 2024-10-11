2041a9b93a94288bc8fe24b40531c188ba9172a0
Chris Baker <bakercw@verizon.net>
2024-10-10 22:43:53 -0400

    Update mtoll.py with function to convert city to grid locator

--- mtoll.py
+++ mtoll.py
@@ -1,8 +1,51 @@
 from flask import Flask, render_template, request
 import maidenhead as mh
 from geopy.distance import geodesic
+from geopy.geocoders import Nominatim
 
 app = Flask(__name__)
+
+
+@app.route('/city', methods=['GET', 'POST'])
+def citytogrid():
+    lat1 = lon1 = lat2 = lon2 = None
+    grid1 = grid2 = distance = None
+    error = None
+
+    if request.method == 'POST':
+        city  = request.form.get('city',  '').strip().upper()
+
+        if city:
+            try:
+
+                # instantiate a new Nominatim client
+                app = Nominatim(user_agent="tutorial")
+
+                # get location raw data
+                location = app.geocode(city).raw
+
+                latitude = location["lat"]
+                longitude = location["lon"]
+                #print(f"Coordinates and grid location for {city}:\n{latitude}, {longitude}")
+
+                # Level Precision
+                # 1     World           AA
+                # 2     Regional        AA11
+                # 3     Metropolis      AA11aa
+                # 4     City            AA11aabb
+                level = 3
+
+                gridloc = mh.to_maiden(float(latitude), float(longitude), level)
+                #print(f"{gridloc}")
+                return render_template('index.html', cityname=city, lat1=latitude, lon1=longitude, lat2=latitude, lon2=longitude, gridc=gridloc, error=error)
+
+            except ValueError as e:
+                error = f"Error: {e}. Please enter valid Maidenhead grid squares."
+                return render_template('index.html', error=error, grid1=grid1, grid2=grid2)
+        
+    return render_template('index.html', lat1=lat1, lon1=lon1, lat2=lat2, lon2=lon2, grid1=grid1, grid2=grid2, distance=distance, error=error)
+        
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -13,6 +56,7 @@ def index():
     if request.method == 'POST':
         grid1 = request.form.get('grid1', '').strip().upper()
         grid2 = request.form.get('grid2', '').strip().upper()
+        
         if not grid1 or not grid2:
             error = "Please enter both grid squares."
             return render_template('index.html', error=error, grid1=grid1, grid2=grid2)
